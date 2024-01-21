8b5eef653f2cadc6eebba9222713751c7af49db2
jahzera <george.m20420@gmail.com>
2024-01-21 12:58:30 +0000

    engine process now created on main executable start up

--- Assets/Scripts/Communication.cs
+++ Assets/Scripts/Communication.cs
@@ -6,11 +6,32 @@ using NetMQ;
 using NetMQ.Sockets;
 using PGNDelegate;
 using PimDeWitte.UnityMainThreadDispatcher;
+using System.Diagnostics; //potential ambiguity with UnityEngine.Debug.Log and System.Diagnostics.Debug.Log
 
 namespace Communication
 {
     public class Client : MonoBehaviour
     {
+        private Process _cobraProcess = null;
+        public void CreateEngineProcess()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "Assets/cobra";
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardInput = true;
+
+            var _cobraProcess = new Process();
+            _cobraProcess.StartInfo = startInfo;
+            _cobraProcess.OutputDataReceived +=
+                (sender, args) =>
+                    UnityEngine.Debug .Log(args.Data); 
+            if (_cobraProcess.Start())
+            {
+                UnityEngine.Debug.Log("Process started");
+            }
+            _cobraProcess.BeginOutputReadLine();
+        }
         // Singleton pattern to easily access the instance
         public static Client Instance { get; private set; }
 
@@ -34,6 +55,7 @@ namespace Communication
 
         private void Start()
         {
+            CreateEngineProcess();
             _pgnExporter = FindObjectOfType<PGNExporter>();
             _requester = new RequestSocket();
             _requester.Connect("tcp://localhost:5555");
@@ -48,7 +70,7 @@ namespace Communication
             {
                 _requester.SendFrame(pgnString);
                 string message = _requester.ReceiveFrameString();
-                Debug.Log("Received: " + message);
+                UnityEngine.Debug.Log("Received: " + message);
 
                 // Use Unity's main thread to call the callback method and to find the Chessboard
                 UnityMainThreadDispatcher.Instance().Enqueue(() =>
@@ -68,6 +90,12 @@ namespace Communication
         
         private void OnDestroy()
         {
+            if (_cobraProcess is { HasExited: false })
+            {
+                _cobraProcess.Kill();
+                _cobraProcess.Dispose();
+            }
+
             _requester?.Dispose();
         }
     }
@@ -90,7 +118,7 @@ namespace Communication
 
                 for (int i = 0; i < 10 && Running; i++)
                 {
-                    Debug.Log("Sending PGN");
+                    UnityEngine.Debug.Log("Sending PGN");
                     client.SendFrame(_pgnString);
 
                     string message = null;
@@ -101,7 +129,7 @@ namespace Communication
                         if (gotMessage) break;
                     }
 
-                    if (gotMessage) Debug.Log("Received " + message);
+                    if (gotMessage) UnityEngine.Debug.Log("Received " + message);
                 }
             }
 
